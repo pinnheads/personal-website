f52ad2b3bb05d248f4a770f4dc344a0a4a83a029
Utsav <utsavdeep01@gmail.com>
2024-01-29 01:05:54 +0530

    Add navbar and ThemeToggle

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		colors: {
+			'teal': '#00E0B0',
+			'black': '#000000',
+			'white': '#F8F8F8',
+			'lg-gray': '#A4A4A4',
+			'da-gray': '#656565'
+		},
+		fontFamily: {
+			sans: ['Inter Variable', 'sans-serif'],
+		},
+		extend: {
+			screens: {
+				'3xl': '1920px',
+			}
+		},
+	},
+	plugins: [
+		require('tailwindcss-animated'),
+	],
+}
