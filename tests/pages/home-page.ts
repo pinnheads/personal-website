1f4cb4e11fa58eca2568e9e8052f7762a30f6379
Utsav <utsavdeep01@gmail.com>
2024-02-10 17:13:28 +0530

    Fix navbar and about section layout

--- tests/pages/home-page.ts
+++ tests/pages/home-page.ts
@@ -7,7 +7,19 @@ export class HomePage {
     public readonly experienceLink: Locator;
     public readonly contactLink: Locator;
     public readonly hamburgerMenu: Locator;
-
+    public readonly introName: Locator;
+    public readonly label: Locator;
+    public readonly summary: Locator;
+    public readonly socials: Locator;
+    public readonly email: Locator;
+    public readonly github: Locator;
+    public readonly linkedin: Locator;
+    public readonly twitter: Locator;
+    public readonly contact: Locator;
+    public readonly resume: Locator;
+    public readonly profileImage: Locator;
+    public readonly skillImageLight: Locator;
+    public readonly skillImageDark: Locator;
 
     constructor(public readonly page: Page) {
         this.brandName = this.page.getByTestId('brand-name');
@@ -16,6 +28,19 @@ export class HomePage {
         this.experienceLink = this.page.getByTestId('section-link-experience');
         this.contactLink = this.page.getByTestId('section-link-contact');
         this.hamburgerMenu = this.page.getByTestId('hamburger-menu');
+        this.introName = this.page.getByTestId('intro-name');
+        this.label = this.page.getByTestId('intro-profession');
+        this.summary = this.page.getByTestId('intro-summary');
+        this.socials = this.page.getByTestId('intro-socials');
+        this.email = this.page.getByTestId('social-email');
+        this.github = this.page.getByTestId('social-github');
+        this.linkedin = this.page.getByTestId('social-linkedin');
+        this.twitter = this.page.getByTestId('social-twitter');
+        this.contact = this.page.getByTestId('contact-btn');
+        this.resume = this.page.getByTestId('resume-btn');
+        this.profileImage = this.page.getByTestId('intro-profile-image');
+        this.skillImageLight = this.page.getByTestId('skills-image-light');
+        this.skillImageDark = this.page.getByTestId('skills-image-dark');
     }
 
     async goto() {
@@ -44,5 +69,25 @@ export class HomePage {
 
     async navigateToHome() {
         await this.brandName.click();
+    }
+
+    async navigateToGithubProfile() {
+        await this.github.click();
+    }
+
+    async navigateToLinkedinProfile() {
+        await this.linkedin.click();
+    }
+
+    async navigateToTwitterProfile() {
+        await this.twitter.click();
+    }
+
+    async navigateToResume() {
+        await this.resume.click();
+    }
+
+    async navigateToContactSection() {
+        await this.contact.click();
     }
 }
