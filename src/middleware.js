f19b515f2a7e6737c0f9e11038ac1849a87d3352
Utsav Deep <utsavdeep01@gmail.com>
2024-02-14 14:36:36 +0530

    Revert "[TAS-25] Add API"

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,9 @@
+import flagsData from '../src/assets/flags.json' assert {type: 'json'};
+
 export async function onRequest ({ locals, request }, next) {
 
-    const requestUrl = import.meta.env.URL ? import.meta.env.URL : 'http://localhost:4321';
-
-    locals.isOn = async (featureName) => {
-        const response = await fetch(`${requestUrl}/flags/${featureName}.json`)
-        const data = await response.json();
-        return data[import.meta.env.ENV];
-    }
-
-    locals.getData = async (data) => {
-        const response = await fetch(`${requestUrl}/resume/${data}.json`)
-        const resumeData = await response.json();
-        return resumeData;
+    locals.isOn = (featureName) => {
+        return flagsData[featureName][import.meta.env.ENV]
     }
 
     // return a Response or the result of calling `next()`
