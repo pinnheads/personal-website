b8fcf26e124f7fe12c007b59c1cda7e4f0792275
Utsav <utsavdeep01@gmail.com>
2024-02-16 21:51:27 +0530

    Add meta tags

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,15 @@
-import flagsData from '../src/assets/flags.json' assert {type: 'json'};
-
 export async function onRequest ({ locals, request }, next) {
 
-    locals.isOn = (featureName) => {
-        return flagsData[featureName][import.meta.env.ENV]
+    locals.isOn = async (featureName) => {
+        const response = await fetch(`${import.meta.env.URL}/flags/${featureName}.json`);
+        const data = await response.json();
+        return data[import.meta.env.ENV];
+    }
+
+    locals.getData = async (resData) => {
+        const response = await fetch(`${import.meta.env.URL}/resume/${resData}.json`);
+        const data = await response.json();
+        return data;
     }
 
     // return a Response or the result of calling `next()`
